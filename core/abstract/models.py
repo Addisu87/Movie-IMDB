2e9c0223c77b5163f3329a721873bf23c49dfa48
Addisu <addisuhaile87@gmail.com>
2024-07-10 20:09:13 +0300

    Fix the issue with logout

--- core/abstract/models.py
+++ core/abstract/models.py
@@ -19,8 +19,8 @@ class AbstractManager(models.Manager):
 class AbstractModel(models.Model):
     public_id = models.UUIDField(
         db_index=True, unique=True, default=uuid.uuid4, editable=False)
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     objects = AbstractManager()
 
--- core/auth/viewsets/logout.py
+++ core/auth/viewsets/logout.py
@@ -1,14 +1,10 @@
-from rest_framework_simplejwt.tokens import (
-    RefreshToken, TokenError
-)
+from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 from rest_framework.response import Response
 from rest_framework import viewsets, status
 from rest_framework.exceptions import ValidationError
-from rest_framework.permissions import IsAuthenticated
 
 
 class LogoutViewSet(viewsets.ViewSet):
-    permission_classes = (IsAuthenticated,)
     http_method_names = ['post']
 
     def create(self, request, *args, **kwargs):
@@ -18,7 +14,7 @@ class LogoutViewSet(viewsets.ViewSet):
             raise ValidationError({'detail': 'Refresh token is required'})
 
         try:
-            token = RefreshToken(request.data.get('refresh'))
+            token = RefreshToken(refresh)
             token.blacklist()
             return Response({'detail': 'Successfully logged out'}, status=status.HTTP_204_NO_CONTENT)
         except TokenError:

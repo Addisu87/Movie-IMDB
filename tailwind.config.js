2ecb24e1d28899d1dc451189a0198b304491ad1e
Addisu <addisuhaile87@gmail.com>
2024-07-14 11:39:46 +0300

    Install tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
 	mode: 'jit',
 	darkMode: 'class', //false, class, media
 	content: [
-		'./core/templates/core/*.html',
+		'./core/templates/**/*.html', // Adjust content path to include all templates
 		'./static/**/*.css',
 		'./static/**/*.js',
 	],
@@ -15,7 +16,20 @@ module.exports = {
 			...defaultTheme.screens,
 		},
 		extend: {
-			colors: {},
+			colors: {
+				transparent: 'transparent',
+				current: 'currentColor',
+				black: colors.black,
+				white: colors.white,
+				gray: colors.neutral,
+				red: colors.red,
+				yellow: colors.amber,
+				green: colors.emerald,
+				blue: colors.blue,
+				indigo: colors.indigo,
+				purple: colors.purple,
+				pink: colors.pink,
+			},
 			fontFamily: {
 				montserrat: ['Montserrat', 'sans-serif'],
 				poppins: ['Poppins', 'sans-serif'],
